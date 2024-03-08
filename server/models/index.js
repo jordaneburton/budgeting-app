4f7d4b136f47e3a3ec96e78402c9daa3bc7ba204
mwg396 <michael.graham1029@gmail.com>
2024-03-08 10:40:36 -0600

    index.js file in models folder

---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const TotalBudget = require('./totalBudget');
+const { Category } = require('./categories');
+const User = require('./user');
+
+module.exports = {
+  TotalBudget,
+  Category,
+  User,
+};
