d20a1f59f46f154cbfa8c2c340ef1e4f827e3629
mwg396 <michael.graham1029@gmail.com>
2024-03-08 10:23:59 -0600

    updated models

--- server/models/Budget.js
+++ server/models/Budget.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const totalBudgetSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+      },
   totalAmount: {
     type: Number,
     required: true,
@@ -14,6 +18,7 @@ const totalBudgetSchema = new mongoose.Schema({
     enum: ['1 month', '2 months', '3 months', '6 months', '1 year'], // Example durations
     required: true,
   },
+  categories: [categorySchema],
   // Other fields related to the total budget if needed
 });
 
