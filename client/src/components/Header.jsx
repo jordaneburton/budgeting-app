baabca7815ccf57beb22f2c1bfcfe66645823fe4
Jordan <jordane.burton@gmail.com>
2024-03-12 02:31:49 -0500

    create pill button component and some beginning for dashboard page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header ({ header }) {
   return (
     <>
-        <h1 className='w100 bg-secondary z-2 ps-2 fw-bold px-md-5 d-flex flex-wrap align-content-center text-start text-white'>
+        <h1 className='w100 bg-secondary z-2 ps-2 fw-bold px-md-5 my-0 d-flex flex-wrap align-content-center text-start text-white'>
             {header}
         </h1>
     </>
